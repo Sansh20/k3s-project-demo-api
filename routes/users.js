430dc55eeaf17da4a31737e14b2e72900a0cdf20
Sanyam Sharma <sanyamsh20@gmail.com>
2022-11-05 20:48:29 +0530

    api-check-trial#1

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const k8s = require('@kubernetes/client-node');
+
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
+
+const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  k8sApi.listNamespacedPod('default').then((resIn) => {
+    res.send(resIn.body);
+});
   res.send('respond with a resource');
 });
 
