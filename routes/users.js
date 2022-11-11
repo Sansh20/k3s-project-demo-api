6bdf6501ff38dccfe3c749db32551f539457d5cd
Sanyam Sharma <sanyamsh20@gmail.com>
2022-11-11 17:18:00 +0530

    Reverted all the changes

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-const k8s = require('@kubernetes/client-node');
-
-const kc = new k8s.KubeConfig();
-kc.loadFromDefault();
-
-const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  k8sApi.listNamespacedPod('default').then((resk) => {
-    res.send(resk.response);
-});
+//   k8sApi.listNamespacedPod('default').then((resk) => {
+//     res.send(resk.response);
+// });
+    res.send("j");
 });
 
 module.exports = router;
